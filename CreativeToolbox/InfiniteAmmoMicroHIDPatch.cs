08ffd82daf5da477aedbd1105bc7272520199ded
DefyTheRush <definitelynotrushing@gmail.com>
2020-05-14 12:34:12 -0400

    CreativeToolbox v1.1
    Fixed:
    - MicroHID's not having Infinite Ammo (Thanks a lot to Sanyae2439 and Virtual (for the information on the Speed and Discharge values and more))

---
+++ CreativeToolbox/InfiniteAmmoMicroHIDPatch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Harmony;
+using Mirror;
+using UnityEngine;
+
+namespace CreativeToolbox
+{
+    [HarmonyPatch(typeof(MicroHID), nameof(MicroHID.UpdateServerside))]
+    class InfiniteAmmoMicrioHIDPatch
+    {
+        public static bool Prefix(MicroHID __instance)
+        {
+            if (__instance.refHub.TryGetComponent(out InfiniteAmmoComponent infAmmo) && __instance.refHub.inventory.curItem == ItemType.MicroHID)
+            {
+                __instance.ChangeEnergy(1);
+                __instance.NetworkEnergy = 1;
+            }
+            return true;
+        }
+    }
+}
